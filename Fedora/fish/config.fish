387b94e21bc0d1288e49ed06ec261afa05bfe4b2
tushar-rishav <tushar.rishav@gmail.com>
2016-08-05 20:07:44 +0530

    tmux.conf: Setup tmux with fish

---
+++ Fedora/fish/config.fish
@@ -0,0 +1,62 @@
+set fish_greeting ""
+bind \ct kill-word
+
+function fish_prompt
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  echo -n ' % '
+end
+
+function dnfi
+  sudo dnf install $argv
+end
+
+function dnfs
+  sudo dnf search $argv
+end
+
+function vimu
+  vim +BundleUpdate
+end
+
+function ydl
+    youtube-dl $argv
+end
+
+function hfind
+    echo $argv
+    find ~/ -name $argv
+end
+
+function rfind
+    sudo find / -name $argv
+end
+
+function gclone
+    git clone $argv
+end
+
+function glog
+    git log --oneline -p $argv
+end
+
+function girebase
+    git rebase -i $argv
+end
+
+function grebase
+    git rebase $argv
+end
+
+function ggrep
+    git grep $argv
+end
+
+function gremote
+    git remote -v
+end
+
+alias vim "nvim"
+
+test -s /home/tushar/.nvm-fish/nvm.fish; and source /home/tushar/.nvm-fish/nvm.fish
